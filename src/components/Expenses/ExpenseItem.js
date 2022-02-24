11aa866ca9b8ded154fa740b76468a257bb7d2b9
Bruno Jullien <bruno.jullien@gmail.com>
2022-02-24 10:34:01 -0800

    50. A Closer Look at the 'useState' Hook

--- src/components/Expenses/ExpenseItem.js
+++ src/components/Expenses/ExpenseItem.js
@@ -1,16 +1,25 @@
-import React from 'react';
+import React, { useState } from 'react';
 import ExpenseDate from './ExpenseDate';
 import Card from '../UI/Card';
 import './ExpenseItem.css';     //importing CSS in JS does not work, but ok in React
 
 const ExpenseItem = (props) => {        //same name as file name, a component is just a function!
+
+  const [title, setTitle] = useState(props.title); //declare state variable and its setter (see below)
+
+  const clickHandler = () => {
+    setTitle("UPDATED!");               //update "title" and call ExpenseItem() component function
+    console.log(title);                 //as change is async, only scheduled, here not yet changed
+  }
+
   return (
     <Card className="expense-item">
       <ExpenseDate date={props.date} />
       <div className="expense-item__description">
-        <h2>{props.title}</h2>
+        <h2>{title}</h2>
         <div className="expense-item__price">${props.amount}</div>
       </div>
+      <button onClick={clickHandler}>Change Title</button>
     </Card>
   );
 }
@@ -30,4 +39,13 @@ export default ExpenseItem;
         - usually named: "props", is an object which contains the list of params as properties
         - e.g. <ExpenseItem title={expenses[0].title} amount={expenses[0].amount} date={expenses[0].date}></ExpenseItem>
         - props properties are the params: title, amount & date
+    - const [title, setTitle] = useState(props.title);
+        - useState() is the so called React hook (useXxxxxx())
+        - hooks always called inside component function, not in a nested function
+        - initial value = props.title, title is variable, setTitle is setter to be used
+        - useState registers a variable as STATE for the current instance of ExpenseItem
+        - when calling setTitle() only one instance is re-evaluated (never use title = .....)
+        - React knows when it is called the first time and then initial value is set to props.title
+        - then when called 2nd, 3rd, .... time, React just use the latest state value
+
 */
